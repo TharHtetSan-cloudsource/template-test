cc118e833eeee5ba7915e88d9c04305e12bd6d08
tharhtetsan <tharhtet1234@gmail.com>
2022-08-17 14:21:09 +0630

    add

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask, request,render_template
+
+app = Flask(__name__,static_url_path='', 
+            static_folder='static',
+            template_folder='templates')
+
+
+@app.get("/")
+def home():
+    return render_template("index.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=False, host='0.0.0.0')
